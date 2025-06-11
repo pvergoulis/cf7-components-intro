201a9a293a8d41696cec1ad95d05bd1966fc8126
pvergoulis <vergoulispaulos@gmail.com>
2025-06-11 20:37:02 +0300

    feat : form validation & errors

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 // import FocusInput from "./components/FocusInput.tsx";
 // import UncontrolledInput from "./components/UncontrolledInput.tsx";
-import MultiFieldForm from "./components/MultiFieldForm.tsx";
+// import MultiFieldForm from "./components/MultiFieldForm.tsx";
+import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
 // import Todo from "./components/Todo/Todo.tsx";
 // import CounterWithReducer from "./components/CounterWithReducer.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
@@ -64,7 +65,7 @@ function App() {
              <Routes>
                 {/*<Route path="/" element={<HomePage/>}/>*/}
                  <Route element={<RouterLayout/>}>
-                     <Route index element={<MultiFieldForm/>}/>
+                     <Route index element={<MultiFieldFormWithValidation/>}/>
                      {/*<Route index element={<HomePage/>}/>*/}
                      {/*<Route index element={<FocusInput/>}/>*/}
                      {/*<Route index element={<UncontrolledInput/>}/>*/}
